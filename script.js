eea33ff99d3688a57f4f4c8501e01670613c7ff6
syedmohammadnuruddin <syedmohammadnuruddin@gmail.com>
2022-06-23 23:48:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function loadPosts(){
+    fetch(`https://restcountries.com/v3.1/all`)
+    .then(res => res.json())
+    .then(posts => displayPost(posts))
+}
+loadPosts()
+
+function displayPost(posts){
+    let postsContainer = document.getElementById("posts");
+    for(let post of posts){
+        singlePost = document.createElement("div");
+        singlePost.classList.add('post');
+        singlePost.innerHTML = `
+            <h3>Country Name: ${post.name.common}</h3>
+            <h3>Population: ${post.population}</h3>
+            <h3> Flag :<img src="${post.flags.svg}" /> </h3>
+        `;
+        postsContainer.appendChild(singlePost);
+    }
+}
